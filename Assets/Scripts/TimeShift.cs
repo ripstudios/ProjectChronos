6103562c9848d0f616f127c556d8a03ee09dda2f
kyuyjay <qyongjian@gmail.com>
2020-02-17 20:13:37 -0500

    Added global timeshift script

---
+++ Assets/Scripts/TimeShift.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeShift : MonoBehaviour
+{
+    public static TimeShift Instance { get; private set; }
+
+    public bool fast = true;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            fast = !fast;
+        }
+    }
+}
