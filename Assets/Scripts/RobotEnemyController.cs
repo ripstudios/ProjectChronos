0b20d4e2fa3b44ede36688cc80a75ce26b248abf
pzupke3 <pzupke3@gatech.edu>
2020-02-25 18:03:43 -0500

    Adding robot enemy to Tutorial

---
+++ Assets/Scripts/RobotEnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotEnemyController : MonoBehaviour
+{
+    public Transform rightGunBone;
+    public GameObject rifle;
+
+    void Awake()
+    {
+        GameObject newRifle = (GameObject)Instantiate(rifle);
+        newRifle.transform.parent = rightGunBone;
+        newRifle.transform.localPosition = Vector3.zero;
+        newRifle.transform.localRotation = Quaternion.Euler(90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
