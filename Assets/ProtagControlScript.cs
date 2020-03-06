c1fae5bfbab36be5b8a689d83ec9b08cb5ca1587
pzupke3 <pzupke3@gatech.edu>
2020-03-06 09:21:12 -0500

    Fixing jump and run movement

--- Assets/ProtagControlScript.cs
+++ Assets/ProtagControlScript.cs
@@ -7,6 +7,9 @@ public class ProtagControlScript : MonoBehaviour
     public float fastSpeed;
     public float slowSpeed;
 
+    private int toggleSpeed;
+    private bool isJumping = false;
+
     private Animator anim;
     // Start is called before the first frame update
     void Start()
@@ -14,20 +17,30 @@ public class ProtagControlScript : MonoBehaviour
         anim = GetComponent<Animator>();
         fastSpeed = 1.0f;
         slowSpeed = 1.0f;
+        toggleSpeed = 9;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateToggleSpeed();
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
-        anim.SetFloat("VSpeed", v);
 
-        this.transform.position = this.transform.position + (v * this.transform.forward * Time.deltaTime * 2);
+        float forwardSpeed = v * toggleSpeed / 3f;
+        anim.SetFloat("VSpeed", forwardSpeed);
+
+        // Work around for jump animation without root motion
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("jump"))
+        {
+            this.transform.position = this.transform.position + (this.transform.forward * Time.deltaTime * forwardSpeed * 1.5f);
+        }
+
+        // TODO: have rotation also covered by root motion animations
         this.transform.rotation = this.transform.rotation * Quaternion.AngleAxis(h * Time.deltaTime * 200, Vector3.up);
 
         // Looks uber weird
-        if (v == 0) { 
+        if (v == 0 && h != 0) { 
             if (h > 0)
             {
                 anim.SetBool("TurningLeft", true);
@@ -36,25 +49,31 @@ public class ProtagControlScript : MonoBehaviour
             {
                 anim.SetBool("TurningRight", true);
             }
-            else
-            {
-                anim.SetBool("TurningLeft", false);
-                anim.SetBool("TurningRight", false);
-            }
+        } else
+        {
+            anim.SetBool("TurningLeft", false);
+            anim.SetBool("TurningRight", false);
         }
-              
+
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log("Jumping");
-            anim.SetBool("Jumping", true);
-            StartCoroutine(Wait(1f));
+            if (!isJumping)
+            {
+                isJumping = true;
+                // Current jumping solution. Apply strong vertical force to rigidbody
+                Rigidbody rb = GetComponent<Rigidbody>();
+                rb.AddForce(new Vector3(0, 150, 0));
+
+                anim.SetBool("Jumping", true);
+            }
+        } else
+        {
             anim.SetBool("Jumping", false);
         }
 
         if (Input.GetKey(KeyCode.Q)) {
             // TODO and check if he has sword
             anim.Play("Attack");
-            Debug.Log("attack!");
         }
 
         // TimeShift functionality
@@ -69,8 +88,62 @@ public class ProtagControlScript : MonoBehaviour
 
     }
 
+    void OnAnimatorMove()
+    {
+        // Currently only use root motion for walking, not jumping
+        // TODO: use root motion animation for all movements
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("jump"))
+        {
+            isJumping = false;
+            this.transform.position = anim.rootPosition;
+        } else
+        {
+            isJumping = true;
+        }
+    }
+
     IEnumerator Wait(float seconds)
     {
         yield return new WaitForSeconds(seconds);
+    }
+
+    void UpdateToggleSpeed()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            toggleSpeed = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            toggleSpeed = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            toggleSpeed = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            toggleSpeed = 4;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            toggleSpeed = 5;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            toggleSpeed = 6;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            toggleSpeed = 7;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            toggleSpeed = 8;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            toggleSpeed = 9;
+        }
     }
 }
