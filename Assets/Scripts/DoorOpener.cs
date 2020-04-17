eef6f4b8d2874086802ef02e7c1640ce6250137e
pzupke3 <pzupke3@gatech.edu>
2020-04-17 15:12:29 -0400

    Modified button logic - blue doors stay open and a button will remain pressed if you stand on it

--- Assets/Scripts/DoorOpener.cs
+++ Assets/Scripts/DoorOpener.cs
@@ -6,18 +6,46 @@ public class DoorOpener : MonoBehaviour
 {
     public GameObject door;
     public float timeOpen = 3.0f;
+    public Material defaultColor;
+    public Material pressedColor;
 
     private Animator anim;
     private TriggeredDoor targetDoor;
+    private Renderer rend;
     private bool unpressed = true;
+    private float pressedCountdown = 0;
+    private float fastIncrement = 10;
+    private float slowIncrement = 1;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         targetDoor = door.GetComponent<TriggeredDoor>();
+        rend = GetComponent<Renderer>();
+        rend.material = defaultColor;
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        if (pressedCountdown > 0)
+        {
+            float increment;
+            if (TimeShift.Instance.fast)
+            {
+                increment = fastIncrement;
+            } else
+            {
+                increment = slowIncrement;
+            }
+            pressedCountdown -= increment;
+
+            if (pressedCountdown <= 0)
+            {
+                UnpressButton();
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider c)
     {
@@ -26,9 +54,9 @@ public class DoorOpener : MonoBehaviour
             anim.SetBool("pressed", true);
             if (unpressed)
             {
+                rend.material = pressedColor;
                 targetDoor.btnsPressed++;
                 unpressed = false;
-                Invoke("CloseDoor", timeOpen);
             }
         }
     }
@@ -38,11 +66,16 @@ public class DoorOpener : MonoBehaviour
         if (c.TryGetComponent(out ProtagControlScript protag))
         {
             anim.SetBool("pressed", false);
+            if (!unpressed)
+            {
+                pressedCountdown = 200;
+            }
         }
     }
 
-    void CloseDoor()
+    void UnpressButton()
     {
+        rend.material = defaultColor;
         unpressed = true;
         targetDoor.btnsPressed--;
     }
