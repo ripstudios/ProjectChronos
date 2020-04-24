6d47f2f2cb65433b5a05adf5af72d565eff529df
kavin <kavinkrishnan@gmail.com>
2020-04-24 14:53:10 -0400

    checkpoint camera fix

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -35,7 +35,36 @@ public class CameraFollow : MonoBehaviour
         height = transform.position.y;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        SetCamera();
     }
+
+    void SetCamera() {
+        if (this.characterToFollow != null)
+        {
+            Transform camPose = this.characterToFollow.transform.Find("CamPos");
+            if (camPose == null)
+            {
+                Debug.LogError("The character is not assigned CamPos.");
+            }
+            else
+            {
+                Vector3 fwd = camPose.TransformDirection(Vector3.forward);
+                Vector3 desiredPosition = new Vector3(camPose.position.x, camPose.position.y, camPose.position.z);
+                Debug.DrawRay(camPose.position, fwd * 1.5f, Color.green);
+                RaycastHit hit;
+                if (Physics.Raycast(camPose.position, fwd, out hit, 1.5f))
+                {
+                    if (hit.collider.gameObject.name != characterToFollow.name && hit.transform.tag != "Door")
+                    {
+                        desiredPosition = this.characterToFollow.transform.Find("FGC_Male_Char_Adam_Rig/mc_Ad_Hip/mc_Ad_Abdomen/mc_Ad_Chest/mc_Ad_Neck/mc_Ad_Head").position;
+                    }
+                }
+                this.transform.position = desiredPosition;
+            }
+        }
+    }
+
 
     private void Update()
     {
