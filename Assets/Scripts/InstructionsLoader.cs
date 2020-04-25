33f5c8e969aa22f5e2c9fb60bdf2e7676a4e2fd2
rtechau3 <rtech153@gmail.com>
2020-04-25 15:53:51 -0400

    main menu screens working

---
+++ Assets/Scripts/InstructionsLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstructionsLoader : MonoBehaviour
+{
+    public void LoadInstructions() {
+        Debug.Log("clicked instructions");
+        SceneManager.LoadScene("Main Menu - Instructions");
+    }
+}
---
+++ Assets/Scripts/IntroTextLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroTextLoader : MonoBehaviour
+{
+    public void LoadIntroText() {
+        SceneManager.LoadScene("Main Menu - Intro Text");
+    }
+}
