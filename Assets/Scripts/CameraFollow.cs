ba47696179334773dd00d9260e22e0948e9de4c1
kavin <kavinkrishnan@gmail.com>
2020-04-27 15:43:41 -0400

    new camera fix

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -40,29 +40,34 @@ public class CameraFollow : MonoBehaviour
     }
 
     void SetCamera() {
-        if (this.characterToFollow != null)
-        {
-            Transform camPose = this.characterToFollow.transform.Find("CamPos");
-            if (camPose == null)
-            {
-                Debug.LogError("The character is not assigned CamPos.");
-            }
-            else
-            {
-                Vector3 fwd = camPose.TransformDirection(Vector3.forward);
-                Vector3 desiredPosition = new Vector3(camPose.position.x, camPose.position.y, camPose.position.z);
-                Debug.DrawRay(camPose.position, fwd * 1.5f, Color.green);
-                RaycastHit hit;
-                if (Physics.Raycast(camPose.position, fwd, out hit, 1.5f))
-                {
-                    if (hit.collider.gameObject.name != characterToFollow.name && hit.transform.tag != "Door")
-                    {
-                        desiredPosition = this.characterToFollow.transform.Find("FGC_Male_Char_Adam_Rig/mc_Ad_Hip/mc_Ad_Abdomen/mc_Ad_Chest/mc_Ad_Neck/mc_Ad_Head").position;
-                    }
-                }
-                this.transform.position = desiredPosition;
-            }
-        }
+        // if (this.characterToFollow != null)
+        // {
+        //     Transform camPose = this.characterToFollow.transform.Find("CamPos");
+        //     if (camPose == null)
+        //     {
+        //         Debug.LogError("The character is not assigned CamPos.");
+        //     }
+        //     else
+        //     {
+        //         Vector3 fwd = camPose.TransformDirection(Vector3.forward);
+        //         Vector3 desiredPosition = new Vector3(camPose.position.x, camPose.position.y, camPose.position.z);
+        //         Debug.DrawRay(camPose.position, fwd * 1.5f, Color.green);
+        //         RaycastHit hit;
+        //         if (Physics.Raycast(camPose.position, fwd, out hit, 1.5f))
+        //         {
+        //             if (hit.collider.gameObject.name != characterToFollow.name && hit.transform.tag != "Door")
+        //             {
+        //                 desiredPosition = this.characterToFollow.transform.Find("FGC_Male_Char_Adam_Rig/mc_Ad_Hip/mc_Ad_Abdomen/mc_Ad_Chest/mc_Ad_Neck/mc_Ad_Head").position;
+        //             }
+        //         }
+        //         this.transform.position = desiredPosition;
+        //     }
+        // }
+
+        Transform camPose = this.characterToFollow.transform.Find("CamPos");
+        Vector3 desiredPosition = new Vector3(camPose.position.x, camPose.position.y, camPose.position.z);
+        this.transform.position = desiredPosition;
+        
     }
 
 
