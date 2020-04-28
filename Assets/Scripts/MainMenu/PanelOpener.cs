11ccb910107441597fd2a5ca242fa210033f0420
rtechau3 <rtech153@gmail.com>
2020-04-27 21:51:40 -0400

    combined main menu screens to one scene

---
+++ Assets/Scripts/MainMenu/PanelOpener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject PanelOpen;
+    public GameObject PanelClose;
+
+    public void OpenPanel() {
+        if (PanelOpen != null) {
+            PanelOpen.SetActive(true);
+        }
+    }
+
+    public void ClosePanel() {
+        if (PanelClose != null) {
+            PanelClose.SetActive(false);
+        }
+    }
+}
